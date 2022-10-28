31484d761845c924f07f7096047953e4e689a033
MUNJU <mvv0030@gmail.com>
2022-10-28 11:33:14 +0900

    spring boot Slf4j Annotation 추가 log.info() 적용

--- src/main/java/com/springboot/hello/controller/GetController.java
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,26 +1,31 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName() {
+        log.info("getName로 요청이 들어왔습니다");
         return "Munju";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable1으로 요청이 들어왔습니다. variable:{}", variable);
         return variable;
     }
 
@@ -36,7 +41,7 @@ public class GetController {
 
     @GetMapping(value = "/request2")
     public String getRequestParam2(@RequestParam Map<String, String> param) {
-        StringBuilder sb = new StringBuilder()
+        StringBuilder sb = new StringBuilder();
         param.entrySet().forEach((map) -> {
             sb.append(map.getKey() + " : " + map.getValue() + "\n");
         });
@@ -47,7 +52,5 @@ public class GetController {
     public String getRequestParam3(MemberDto memberDto) {
         return memberDto.toString();
     }
-
-
 
 }
