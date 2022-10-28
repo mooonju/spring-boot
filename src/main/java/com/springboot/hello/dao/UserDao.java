3a8f15091f15c79b07ab59e74e16ec3fa7a43931
MUNJU <mvv0030@gmail.com>
2022-10-28 15:06:33 +0900

    UserDao add deleteAll deleteById 추가

--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -18,37 +18,19 @@ public class UserDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    RowMapper<User> rowMapper = new RowMapper<User>() {
-        @Override
-        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-            return user;
-        }
-    };
 
     public int deleteAll() {
         return this.jdbcTemplate.update("delete from users");
     }
 
     public void add(User user){
-        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
-                user.getId(), user.getName(), user.getPassword());
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)", user.getId(), user.getName(), user.getPassword());
+
     }
 
 
-    public User findById(String id) {
-        String sql = "select * from users where id = ?";
-        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
+    public void deleteById(String id){
+        this.jdbcTemplate.update("delete from users where id = ?", id);
     }
-
-    public int getCount() throws SQLException {
-        return this.jdbcTemplate.queryForObject("select count(*) from users;", Integer.class);
-    }
-
-    public List<User> getAll() {
-        String sql = "select * from users order by id";
-        return this.jdbcTemplate.query(sql, rowMapper);
-    }
-
 
 }
